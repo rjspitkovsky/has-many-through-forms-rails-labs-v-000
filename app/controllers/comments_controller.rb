91c723fdd2c2b022f49fc4e06248b949f7495c75
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-13 18:45:53 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@ class CommentsController < ApplicationController
 
 
   def create
-    @comment = Comment.create(comment_params)
-    redirect_to @comment.post
+    comment = Comment.create(comment_params)
+    redirect_to comment.post
   end
 
   private
