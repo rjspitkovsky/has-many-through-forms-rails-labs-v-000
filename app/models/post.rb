00c7cca37b8b4e8d5a94eaf9c9d0686322bcdbfc
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-13 18:12:23 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ActiveRecord::Base
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
     end
+  end
+
+  def unique_users
+    self.comments.collect{|comment|comment.user}.uniq
   end 
 
 
