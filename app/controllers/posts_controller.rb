0da48d09afb790a78f588b905afe64dec3d9ff05
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-13 18:04:23 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post_path(post)
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private
